91f556ffabce4aab9f1c2906a7ab2c10652c4df9
sunbean <sunbeansoft@gmail.com>
2015-11-18 20:17:41 +0800

    添加cglibdemo

--- src/main/java/com/fun/sb/demo/cg/impl/BookFacadeImpl1.java
+++ src/main/java/com/fun/sb/demo/cg/impl/BookFacadeImpl1.java
@@ -1,11 +1,13 @@
 package com.fun.sb.demo.cg.impl;
 
+import java.io.Serializable;
+
 /**
  * 这个是没有实现接口的实现类
  *
  * @author student
  */
-public class BookFacadeImpl1 {
+public class BookFacadeImpl1 implements Serializable {
     public void addBook() {
         System.out.println("增加图书的普通方法...");
     }
--- src/main/java/com/fun/sb/demo/cg/impl/TestCglib.java
+++ src/main/java/com/fun/sb/demo/cg/impl/TestCglib.java
@@ -5,6 +5,7 @@ public class TestCglib {
     public static void main(String[] args) {
         BookFacadeCglib cglib = new BookFacadeCglib();
         BookFacadeImpl1 bookCglib = (BookFacadeImpl1) cglib.getInstance(new BookFacadeImpl1());
+        System.out.println(bookCglib);
         bookCglib.addBook();
         bookCglib.aaa();
     }
