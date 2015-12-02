617f94c9169af43b2ae17501e28c496e610a89fc
sunbean <sunbeansoft@gmail.com>
2015-12-02 14:11:54 +0800

    添加cglibdemo

--- src/main/java/com/fun/sb/demo/cg/impl/TestCglib.java
+++ src/main/java/com/fun/sb/demo/cg/impl/TestCglib.java
@@ -1,8 +1,11 @@
 package com.fun.sb.demo.cg.impl;
 
+import net.sf.cglib.core.DebuggingClassWriter;
+
 public class TestCglib {
 
     public static void main(String[] args) {
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "D:\\");
         BookFacadeCglib cglib = new BookFacadeCglib();
         BookFacadeImpl1 bookCglib = (BookFacadeImpl1) cglib.getInstance(new BookFacadeImpl1());
         System.out.println(bookCglib);
